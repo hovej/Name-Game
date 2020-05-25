5cb7169a56d3bfdfc8c862bc3e142cd870c5c0a8
Justin Hove <hovej88@gmail.com>
2020-05-25 17:13:36 -0500

    Updates

---
+++ precache-manifest.9beb6ac5bd95a3dcc2c556d15f2e2a7a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c01478641ec100e0e2611d40774af4b6",
+    "url": "/Name-Game/index.html"
+  },
+  {
+    "revision": "f72ff9529c1521ca8d6e",
+    "url": "/Name-Game/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "b434cd7f7c59df99b192",
+    "url": "/Name-Game/static/js/2.53b2b867.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Name-Game/static/js/2.53b2b867.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f72ff9529c1521ca8d6e",
+    "url": "/Name-Game/static/js/main.5555d12b.chunk.js"
+  },
+  {
+    "revision": "d01cac9f4f4fe116fa07",
+    "url": "/Name-Game/static/js/runtime-main.3209c038.js"
+  }
+]);
